d1808507e495ac3c276a0fc9c51e42f4f54ebe19
er1cyao <104165512+er1cyao@users.noreply.github.com>
2026-07-19 14:53:45 -0400

    cleanup

--- src/main/java/com/eryao/financeapi/FinanceapiApplication.java
+++ src/main/java/com/eryao/financeapi/FinanceapiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinanceapiApplication {
+public class FinanceApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FinanceapiApplication.class, args);
+		SpringApplication.run(FinanceApiApplication.class, args);
 	}
 
 }
